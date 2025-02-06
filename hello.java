6e48540265cea08ca302711b9851bc7a176c2945
Chimegerdene Drl <Lenovo@DESKTOP-DG7QGI5.(none)>
2025-02-06 20:06:42 +0800

    sorting algo added

--- hello.java
+++ hello.java
@@ -1,9 +1,8 @@
 
-public class hello {
+public class Hello {
 
     public static void main(final String[] args) {
         System.out.println("Hello World!");
         System.out.println("Testing github");
     }
-
 }
--- my-app/src/main/java/com/example/Main.java
+++ my-app/src/main/java/com/example/Main.java
@@ -1,7 +1,33 @@
 package com.example;
 
+import java.util.Arrays;
+
+/**
+ * Sort array.
+ */
 public class Main {
+
+    /**
+     * main function .
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int[] arr = {2, 5, 3, 1, 6};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+
+        }
+
+        System.out.println("Array sorted: " + Arrays.toString(arr));
     }
 }
