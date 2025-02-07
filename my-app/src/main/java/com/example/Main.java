a6fc2d21dc4e026d5ed1bfb6d41026cabc9e1875
Chimegerdene Drl <Lenovo@DESKTOP-DG7QGI5.(none)>
2025-02-07 15:41:56 +0800

    test tried

--- my-app/src/main/java/com/example/Main.java
+++ my-app/src/main/java/com/example/Main.java
@@ -7,14 +7,7 @@ import java.util.Arrays;
  */
 public class Main {
 
-    /**
-     * main function .
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        int[] arr = {2, 5, 3, 1, 6};
-
+    public static int[] sort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             int min = i;
             for (int j = i + 1; j < arr.length; j++) {
@@ -28,6 +21,17 @@ public class Main {
 
         }
 
-        System.out.println("Array sorted: " + Arrays.toString(arr));
+        return arr;
+    }
+
+    /**
+     * main function .
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int[] array = {2, 5, 3, 1, 6};
+
+        System.out.println("Array sorted: " + Arrays.toString(sort(array)));
     }
 }
---
+++ my-app/src/test/Maintest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+// import com.example.Main; 
+class MainTest {
+
+    // @Test
+    // void testSort() {
+    //     int[] input = {2, 5, 3, 1, 6};
+    //     int[] expected = {1, 2, 3, 5, 6};
+    //     assertArrayEquals(expected, Main.sort(input), "Incorrect.");
+    // }
+}
